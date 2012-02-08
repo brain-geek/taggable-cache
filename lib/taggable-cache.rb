ea3f1cd7bab8101d9cfc2e71f2dcfbf2e7a0e558
Alex Rozumey <brain-geek@yandex.ua>
2012-02-08 20:12:22 +0200

    Added slugging objects

--- lib/taggable-cache.rb
+++ lib/taggable-cache.rb
@@ -1,10 +1,21 @@
 require "redis"
 
-
 class TaggableCache
   def initialize
     @redis = Redis.new
   end
 
-  
+  def id_for(obj)
+    if obj.is_a? Class
+      obj.to_s.downcase
+    elsif obj.is_a? ActiveRecord::Base
+      if obj.persisted?
+        "#{obj.class.to_s.downcase}-#{obj.id}" 
+      else
+        id_for(obj.class)
+      end
+    else
+      nil
+    end
+  end
 end
--- spec/taggable-cache_spec.rb
+++ spec/taggable-cache_spec.rb
@@ -1,10 +1,39 @@
 require 'spec_helper'
 
 describe TaggableCache do
-  describe "should connect to redis" do
+  describe "connection to redis" do
   	it "should use default settings" do
       Redis.should_receive(:new)
       TaggableCache.new
   	end
+  end
+
+  describe "adding tags" do
+    before :all do
+      @object = TaggableCache.new
+    end
+    describe "queque names" do
+      it "should use id and model name if saved activerecord object" do
+        @object.id_for(Page.create(:id => 1)).should == 'page-1'
+      end
+
+      it "should use id and model name if not saved activerecord object" do
+        @object.id_for(Page.new(:id => 1)).should == 'page'
+      end
+
+      it "should use model name if model is passed" do
+        @object.id_for(Page).should == 'page'
+      end
+
+      it "should return nil if unknown is passed" do
+        @object.id_for(123).should be_nil
+      end
+    end
+
+    pending "adding data to redis" do
+      it "should push data" do
+
+      end
+    end
   end
 end
