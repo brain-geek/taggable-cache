17d5f7f9f420b2feed7642ec0a669516796ee218
Alex Rozumey <brain-geek@yandex.ua>
2012-06-03 18:31:09 +0300

    Add additional hack to make it work with redis.

--- lib/taggable_cache/railtie.rb
+++ lib/taggable_cache/railtie.rb
@@ -5,8 +5,10 @@ module TaggableCache
     initializer "taggable_cache" do |app|
       ActiveSupport.on_load(:active_record) do
         ::ActiveRecord::Base.send :include, TaggableCache::ActiveRecordExtension
-        ::ActiveSupport::Cache::Store.send :include, TaggableCache::CacheStoreExtension
         ::ActionController::Base.send :include, TaggableCache::ActionControllerExtension        
+
+        ::ActiveSupport::Cache::Store.send :include, TaggableCache::CacheStoreExtension
+        ::ActiveSupport::Cache::RedisStore.send :include, TaggableCache::CacheStoreExtension
       end
     end
   end  
