ffe492b997c62c72eaa10a807fa203760e187c84
Alex Rozumey <brain-geek@yandex.ua>
2012-06-30 16:28:18 +0300

    Version bump

--- lib/taggable_cache/version.rb
+++ lib/taggable_cache/version.rb
@@ -1,3 +1,3 @@
 module TaggableCache
-  VERSION = "0.4.1" # This is for the gem and does not conflict with the rest of the functionality
+  VERSION = "0.4.2" # This is for the gem and does not conflict with the rest of the functionality
 end
